44d0e71676da591c2f761b699391355b5c9d3d69
Scarlett Clark <sgclark@kde.org>
2016-07-02 06:18:43 -0700

    Add license and ruby shebang to script.

--- builddocker.rb
+++ builddocker.rb
@@ -1,3 +1,25 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+# 
+# Copyright (C) 2016 Scarlett Clark <sgclark@kde.org>
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'docker'
 require 'logger'
 require 'logger/colors'
@@ -83,7 +105,7 @@ appimage.name = "ark"
 appimage.version = '16.04.1'
 #TO_DO do some LD magic here? kdev-tools cmake parser?
 appimage.depends = 'bzip2-devel liblzma-devel'
-appimage.frameworks = 'extra-cmake-modules karchive kconfig kwidgetsaddons kcompletion kcoreaddons kauth kcodecs kdoctools kguiaddons ki18n kconfigwidgets kwindowsystem kcrash kdbusaddons kitemviews kiconthemes kjobwidgets kservice solid sonnet ktextwidgets attica kglobalaccel kxmlgui kbookmarks kio knotifications kparts kpty kwayland'
+appimage.frameworks = 'karchive kconfig kwidgetsaddons kcompletion kcoreaddons kauth kcodecs kdoctools kguiaddons ki18n kconfigwidgets kwindowsystem kcrash kdbusaddons kitemviews kiconthemes kjobwidgets kservice solid sonnet ktextwidgets attica kglobalaccel kxmlgui kbookmarks kio knotifications kparts kpty kwayland'
 appimage.apps = [Recipe::App.new("#{appimage.name}")]
 File.write('Recipe', appimage.render)
 
